3ec0dcc00d5c1e4f42aaf677f77422ad4885f6fd
Mohammad-Quanit <muhammadquanit@gmail.com>
2023-10-30 15:52:58 +0500

    (add): remove product functionality

--- product/controllers/removeProduct.go
+++ product/controllers/removeProduct.go
@@ -1,7 +1,22 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/mohammad-quanit/Go-Microservices-App/product/models"
+)
+
+// DELETE /products/:id
+// Delete a product
 func RemoveProduct(c *gin.Context) {
-	c.JSON(200, gin.H{"ping": "pong"})
+	var product models.Product
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&product).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&product)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
